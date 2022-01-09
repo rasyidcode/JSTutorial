67b10c5bcf26ab5032175e85c1124b21c9573f69
Jamil Al Rasyid <jamil@dwebsite.net>
2022-01-09 18:04:20 +0700

    re init kulineran with vue 2

--- kulineran/src/main.js
+++ kulineran/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(BootstrapVue)
+    .use(BootstrapVueIcons)
+    .mount('#app')
